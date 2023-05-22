41ae4c5ea34d7d6a490273888033f8d1bd18bd95
Farul Wananda <farulahmad2002@gmail.com>
2023-05-23 00:30:20 +0700

    feat: sequelize migration user

---
+++ Access-API/migrations/20230522142824-users-table.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { QueryInterface } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      favorite_food: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      budget: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      }
+    });
+
+    await queryInterface.addConstraint('users', {
+      type: 'unique',
+      fields: ['email'],
+      name: 'UNIQUE_FIELDS'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
