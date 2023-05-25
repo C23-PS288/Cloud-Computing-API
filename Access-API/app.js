cb6734ed4435cc2f270e3dd2a711ff2c2e52bd1e
Farul Wananda <farulahmad2002@gmail.com>
2023-05-25 14:01:49 +0700

    fix: change port

--- Access-API/app.js
+++ Access-API/app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const router = require('./routes/routes');
 
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '8080';
 
 app.use(logger('dev'));
 app.use(express.json());
