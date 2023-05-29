831bac6af1c34764f6abbf36160b2411647864e9
Farul Wananda <farulahmad2002@gmail.com>
2023-05-29 23:00:27 +0700

    feat: get user by id

--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,10 @@ const authenticateToken = require('../middleware/checkMiddleware');
 
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.get('/:id', usersHandler.getUser);
 router.post('/logout', usersHandler.logout);
 router.get('/protected', authenticateToken, (req, res) => {
-  // Akses data pengguna yang ada di req.userData
   const userId = req.user.id;
-
-  // Melakukan logika atau aksi yang diperlukan
-  // ...
 
   res.json({
     message: 'Protected route accessed successfully',
