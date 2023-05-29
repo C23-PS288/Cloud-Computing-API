2968cc5a29a4c903cc2d231b937accbb8e05c892
Farul Wananda <farulahmad2002@gmail.com>
2023-05-29 13:47:56 +0700

    feat: middleware check

---
+++ middleware/checkMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({
+      message: 'Forbidden',
+    });
+  }
+};
