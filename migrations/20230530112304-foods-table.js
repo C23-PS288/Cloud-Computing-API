bcbc0180a962d44cd5186cda3264dbf58a019bc3
Farul Wananda <farulahmad2002@gmail.com>
2023-05-30 18:53:59 +0700

    feat: foods database migration

---
+++ migrations/20230530112304-foods-table.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.createTable('foods', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    nama_warung: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    menu: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    kategori: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    harga: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    menu_pic: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+   });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('foods');
+  }
+};
