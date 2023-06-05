37ba640182624082cd880f0d5005596c47845db5
Farul Wananda <farulahmad2002@gmail.com>
2023-06-05 23:40:19 +0700

    feat: access token cookies middleware

--- middleware/checkMiddleware.js
+++ middleware/checkMiddleware.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
-
+  const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1] ||
+  req.cookies.accessToken;
+  
   if (!token) {
     return res.status(401).json({
       message: 'Unauthorized',
