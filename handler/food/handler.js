b32e78de1910dcc8f52464a4b776a8d7d1b7d8d1
Farul Wananda <farulahmad2002@gmail.com>
2023-06-10 15:58:09 +0700

    feat: get food by id

--- handler/food/handler.js
+++ handler/food/handler.js
@@ -1,5 +1,36 @@
 const { Food } = require('../../models');
 
+
+async function getFood(req, res) {
+  const id = req.params.id;
+  try {
+    const food = await Food.findByPk(id, {
+      attributes: ['id', 'nama_warung', 'menu', 'kategori', 'harga', 'menu_pic']
+    });
+
+    if (!food) {
+      return res.status(404).json({
+        code: '404',
+        status: 'error',
+        message: 'food not found'
+      });
+    }
+
+    return res.status(200).json({
+      code: '200',
+      status: 'success',
+      data: food,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      code: '500',
+      status: 'error',
+      message: 'internal server error'
+    });
+  }
+}
+
 async function getAllFood(req, res) {
   try {
     const food = await Food.findAll({
@@ -22,5 +53,6 @@ async function getAllFood(req, res) {
 }
 
 module.exports = {
+  getFood,
   getAllFood,
 };
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { 
+  getFood,
   getAllFood
 } = require('../handler/food/handler');
 
 router.get('/', getAllFood);
+router.get('/:id', getFood);
 
 module.exports = router;
